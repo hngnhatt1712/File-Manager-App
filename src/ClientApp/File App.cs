cf7f2a6b4d5487713cb4c9ed8854170b40d82766
DaoNhat <daonhat1712@gmail.com>
2025-11-09 19:56:13 +0700

    Source code

---
+++ src/ClientApp/File App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    public partial class File_App : Form
+    {
+        public File_App()
+        {
+            InitializeComponent();
+        }
+    }
+}
