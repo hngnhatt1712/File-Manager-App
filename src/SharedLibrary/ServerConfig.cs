7b4a4716fcd8232a9a5c30ba8f7bdefa0f2573d8
DaoNhat <daonhat1712@gmail.com>
2025-12-04 18:12:55 +0700

    Cố định IP Server

---
+++ src/SharedLibrary/ServerConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public static class ServerConfig
+    {
+        public const string SERVER_IP = "127.0.0.1"; // Khi demo thì thay bằng IP thật của máy đại diện
+        public const int PORT = 8888;
+    }
+}
