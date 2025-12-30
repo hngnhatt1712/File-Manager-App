390dbfef6697d49e4f70d10ea6df1da61642fec9
24521485 <24521485@gm.uit.edu.vn>
2025-12-30 06:59:08 -0800

    themchitiet

--- src/ClientApp/Forms UI/Setting.Designer.cs
+++ src/ClientApp/Forms UI/Setting.Designer.cs
@@ -28,46 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
             label1 = new Label();
-            panel1.SuspendLayout();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(label1);
-            panel1.Location = new Point(2, 4);
-            panel1.Margin = new Padding(2, 2, 2, 2);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(395, 524);
-            panel1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(278, 183);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Font = new Font("Segoe UI Semibold", 20F, FontStyle.Bold);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(234, 17);
             label1.Name = "label1";
-            label1.Size = new Size(56, 20);
+            label1.Size = new Size(363, 37);
             label1.TabIndex = 0;
-            label1.Text = "Cai dat";
+            label1.Text = "Thông Tin File App Manager";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F);
+            label2.Location = new Point(96, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14.25F);
+            label3.Location = new Point(63, 169);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Gainsboro;
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(234, 99);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(393, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Gainsboro;
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(234, 171);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(393, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.PeachPuff;
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(466, 254);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 44);
+            button1.TabIndex = 5;
+            button1.Text = "Đổi mật khẩu";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.PeachPuff;
+            button2.Font = new Font("Segoe UI", 12F);
+            button2.ForeColor = Color.Red;
+            button2.Location = new Point(234, 254);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 44);
+            button2.TabIndex = 6;
+            button2.Text = "Thông tin App";
+            button2.UseVisualStyleBackColor = false;
             // 
             // Setting
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(panel1);
-            Margin = new Padding(2, 2, 2, 2);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(2);
             Name = "Setting";
-            Size = new Size(891, 435);
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            Size = new Size(780, 326);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Panel panel1;
         private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
     }
 }
